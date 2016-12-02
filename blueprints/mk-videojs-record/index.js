86e97a85ec9e3db1fc2f27aecb2a49d10e82d731
Dmytro Borchuk <Dmytro.Borchuk@symphony-solutions.eu>
2016-12-02 15:56:19 +0200

    Remove some strings

--- blueprints/mk-videojs-record/index.js
+++ blueprints/mk-videojs-record/index.js
@@ -2,10 +2,7 @@ module.exports = {
   description: 'mk-videojs-record',
 
   afterInstall: function() {
-    // this.addBowerPackageToProject('video.js', '~5.12');
-    this.addBowerPackageToProject('mc-videojs-record', '1.5.1');
-    // return this.addBowerPackageToProject('videojs-record', '^1.5.0');
-    return this.addBowerPackageToProject('git@github.com:multicora/videojs-record.git');
+    return this.addBowerPackageToProject('mc-videojs-record', '1.5.1');
   },
 
   normalizeEntityName: function() {
